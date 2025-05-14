cc26ce6bad153a78d71e2335be67a1d7a4a232f3
JE Zingade <jna.zingade@gmail.com>
2025-05-14 17:27:15 +0530

    2025 year added

--- frontend/src/components/Utils/CommonConstants.js
+++ frontend/src/components/Utils/CommonConstants.js
@@ -176,5 +176,5 @@ export const LAST_MONTH_AMOUNT_VALUE = "2";
 export const TOTAL_AMOUNT_VALUE = "3";
 export const BATCH_BUMBER_VALUE = "4";
 export var currentSortYear = {
-  value:'2024',
+  value:'2025',
 };
--- frontend/src/components/Utils/SortYear.js
+++ frontend/src/components/Utils/SortYear.js
@@ -45,6 +45,7 @@ const SortYear = (props) => {
                 <MenuItem value={'2022'}>2022</MenuItem>
                 <MenuItem value={'2023'}>2023</MenuItem>
                 <MenuItem value={'2024'}>2024</MenuItem>
+                <MenuItem value={'2025'}>2025</MenuItem>
             </Select>
             </FormControl>
         </Grid>
